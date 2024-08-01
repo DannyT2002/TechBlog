294049f347b41dd60fca94b430a2a92f124ae799
Daniel Torres <torres.daniel.0827@gmail.com>
2024-07-31 21:26:01 -0500

    fixe connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,  // Change this to an environment variable
     dialect: 'postgres',
-    port: 5432
+    port: process.env.DB_PORT || 5432  // Default to 5432 if DB_PORT is not set
   }
 );
 
